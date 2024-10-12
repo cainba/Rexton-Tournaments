fe02afa8fb7115b55704f592488c5d0114f69eac
Bryce <bryce.cain@debtconnects.com>
2024-10-12 01:51:45 -0600

    Updated routes
    Signed-off-by: Bryce <bryce.cain@debtconnects.com>

--- app.ts
+++ app.ts
@@ -1,27 +1,39 @@
+import type { BunFile } from "bun"
+
+const serverDir = "/var/www/rxt/"
 Bun.serve({
-    fetch(request) {
-        const reqURL = new URL(request.url)
-        if(reqURL.pathname === '/hello') {
-            return new Response(Bun.file("index.html"), {
+    async fetch(request) {
+        const reqURL: URL = new URL(request.url)
+        const reqFileName: string = reqURL.pathname.split("/").pop() || ""
+        const reqFileSource: BunFile  = Bun.file(serverDir + reqFileName)
+        if (reqFileName != "" && await reqFileSource.exists()) {
+            return new Response(reqFileSource, {
                 headers: {
-                    "Content-Type": "text/html"
+                    "Content-Type": reqFileSource.type
                 }
             })
         }
+        return new Response("Not Found", {
+            status: 404,
+            statusText: "Not Found",
+            headers: {
+                "Content-Type": "text/plain"
+            }
+        })
     },
-    port:443,
-    hostname:"0.0.0.0",
+    port: 443,
+    hostname: "0.0.0.0",
     tls: {
-        key:Bun.file("/etc/rxt/ssl/rxt_key.pem"),
-        cert:Bun.file("/etc/rxt/ssl/rxt_cert.pem"),
+        key: Bun.file("/etc/rxt/ssl/rxt_key.pem"),
+        cert: Bun.file("/etc/rxt/ssl/rxt_cert.pem"),
         serverName: "rextongaming.com"
     },
     websocket: {
-       open(ws) {
+        open(ws) {
 
-       },
-         message(ws, message) {
-              ws.send(message instanceof Buffer ? new Uint8Array(message) : message)
-         },
+        },
+        message(ws, message) {
+            ws.send(message instanceof Buffer ? new Uint8Array(message) : message)
+        },
     }
 })
